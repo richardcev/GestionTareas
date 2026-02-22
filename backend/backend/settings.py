cf729d0ff9e096f2ad592c7a2dc57abcf9328b2e
David Cevallos <ridaceva@espol.edu.ec>
2026-02-21 23:42:21 -0500

    change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,4 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://gestion-tareas-dun.vercel.app"
 ]
